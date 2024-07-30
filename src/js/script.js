45fd6607bd6e396f27773606a4135d7585bbe888
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-07-30 13:17:03 +0330

    console

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
-let textElem = document.getElementsByClassName("text")[0]
-let titleElem = document.getElementById("title")
-console.log(content)
+// let textElem = document.getElementsByClassName("text")[0]
+// let titleElem = document.getElementById("title")
+// console.log(content)
+
+
