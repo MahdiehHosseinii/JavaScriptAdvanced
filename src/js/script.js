de7ed011fd52302d8324db030fb604a8d75cce0d
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-07-30 19:12:29 +0330

    Auto Complete Box project

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,52 @@
 // let titleElem = document.getElementById("title")
 // console.log(content)
 
+let $ = document
+let autoCompleteWrapper = $.querySelector(".search-input")
+let searchInputElem = $.querySelector("input")
+let autoCompleteBox = $.querySelector(".autocom-box")
+
+searchInputElem.addEventListener("keyup", function () {
+    let searchValue = searchInputElem.value
+
+    if (searchValue) {
+        autoCompleteWrapper.classList.add("active")
+
+        let filteredWords = suggestions.filter(function (word) {
+            return word.toLowerCase().startsWith(searchValue.toLowerCase())
+        })
+
+        suggestionWordsGenerator(filteredWords)
+
+    } else {
+        autoCompleteWrapper.classList.remove("active")
+    }
+})
+
+function suggestionWordsGenerator(wordsArray) {
+    let listItemsArray = wordsArray.map(function (word) {
+        return "<li>" + word + "</li>"
+    })
+
+    let customListItem
+    if (!listItemsArray.length) {
+        customListItem = "<li>" + searchInputElem.value + "</li>"
+    } else {
+        customListItem = listItemsArray.join("")
+    }
+
+    autoCompleteBox.innerHTML = customListItem
+
+    select()
+}
+
+function select() {
+    let allListItems = autoCompleteBox.querySelectorAll("li")
+    allListItems.forEach(function (wordItem) {
+        wordItem.addEventListener("click", function (event) {
+            searchInputElem.value = event.target.textContent
+            autoCompleteWrapper.classList.remove("active")
+        })
+    })
+}
 
