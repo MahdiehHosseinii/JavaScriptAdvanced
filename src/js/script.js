fcf68816a15fab994148dcc07ccaa83f25b7f849
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-08-06 13:39:06 +0330

    callback function

--- src/js/script.js
+++ src/js/script.js
@@ -109,60 +109,87 @@
 //     }
 // })
 
-let $ = document
+// let $ = document
+//
+// const inputElem = $.querySelector('#input-field')
+// const btnSaveElem = $.querySelector('#btn-save')
+// const btnDeleteElem = $.querySelector('#btn-delete')
+// const colorsBox = $.querySelectorAll('.color-box')
+// const notesContainer = $.querySelector('#listed')
+//
+// notesContainer.addEventListener('click', function (event) {
+//     if (event.target.tagName === 'P') {
+//         event.target.parentElement.remove()
+//     }
+// })
+//
+// colorsBox.forEach(function (colorBox) {
+//     colorBox.addEventListener('click', function (event) {
+//         let mainColor = event.target.style.backgroundColor
+//         inputElem.style.backgroundColor = mainColor
+//     })
+// })
+//
+// function generateNewNote() {
+//     let newNoteDivElem = $.createElement('div')
+//     newNoteDivElem.className = 'card shadow-sm rounded'
+//     let inputBg = inputElem.style.backgroundColor
+//     newNoteDivElem.style.backgroundColor = inputBg
+//
+//     let newNotePElem = $.createElement('p')
+//     newNotePElem.className = 'card-text p-3'
+//     newNotePElem.innerHTML = inputElem.value
+//
+//     newNoteDivElem.append(newNotePElem)
+//
+//     notesContainer.append(newNoteDivElem)
+//
+//     inputElem.value = ''
+//     inputElem.style.backgroundColor = '#fff'
+// }
+//
+// btnDeleteElem.addEventListener('click', function () {
+//     inputElem.value = ''
+//     inputElem.style.backgroundColor = '#fff'
+// })
+//
+// inputElem.addEventListener('keydown', function (event) {
+//     if (event.keyCode === 13) {
+//         if (inputElem.value) { // 'dsv' '' ' '
+//             generateNewNote()
+//         }
+//
+//     }
+// })
+//
+// btnSaveElem.addEventListener('click', generateNewNote)
 
-const inputElem = $.querySelector('#input-field')
-const btnSaveElem = $.querySelector('#btn-save')
-const btnDeleteElem = $.querySelector('#btn-delete')
-const colorsBox = $.querySelectorAll('.color-box')
-const notesContainer = $.querySelector('#listed')
 
-notesContainer.addEventListener('click', function (event) {
-    if (event.target.tagName === 'P') {
-        event.target.parentElement.remove()
-    }
-})
+const btn = document.querySelector("button")
 
-colorsBox.forEach(function (colorBox) {
-    colorBox.addEventListener('click', function (event) {
-        let mainColor = event.target.style.backgroundColor
-        inputElem.style.backgroundColor = mainColor
-    })
-})
-
-function generateNewNote() {
-    let newNoteDivElem = $.createElement('div')
-    newNoteDivElem.className = 'card shadow-sm rounded'
-    let inputBg = inputElem.style.backgroundColor
-    newNoteDivElem.style.backgroundColor = inputBg
-
-    let newNotePElem = $.createElement('p')
-    newNotePElem.className = 'card-text p-3'
-    newNotePElem.innerHTML = inputElem.value
-
-    newNoteDivElem.append(newNotePElem)
-
-    notesContainer.append(newNoteDivElem)
-
-    inputElem.value = ''
-    inputElem.style.backgroundColor = '#fff'
+function logger(callbackFunc) {
+    console.log("logged (1)")
+    callbackFunc()
 }
 
-btnDeleteElem.addEventListener('click', function () {
-    inputElem.value = ''
-    inputElem.style.backgroundColor = '#fff'
+function logger2() {
+    console.log("logged (2)")
+}
+
+logger(logger2)
+
+function clickHandler() {
+    console.log("clicked")
+}
+
+btn.addEventListener("click", clickHandler)
+let numbers = [1, 2, 3, 4, 5, 6]
+
+numbers.map(function (item) {
+    console.log(item)
 })
 
-inputElem.addEventListener('keydown', function (event) {
-    if (event.keyCode === 13) {
-        if (inputElem.value) { // 'dsv' '' ' '
-            generateNewNote()
-        }
 
-    }
-})
-
-btnSaveElem.addEventListener('click', generateNewNote)
 
 
 
