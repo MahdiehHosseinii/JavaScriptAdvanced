f26b3270db406f0d6784fa7b15faa0a696b58619
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-08-17 13:09:25 +0330

    rest operator

--- src/js/script.js
+++ src/js/script.js
@@ -430,16 +430,28 @@
 // console.log("new user: ", newUser)
 
 
-function sum(a, b, c) {
-    // console.log("a", a)
-    // console.log("b", b)
-    // console.log("c", c)
-    //
-    // console.log(arguments)
+// function sum  (a, b, c) {
+// console.log("a", a)
+// console.log("b", b)
+// console.log("c", c)
+//
+// console.log(arguments)
+//
+//     let sumAllArgs = 0
+//
+//     Array.from(arguments).forEach(item => sumAllArgs += item)
+//
+//     return sumAllArgs
+// }
+//
+// console.log(sum(2, 1, 4, 5, 2, 10))
+
+const sum = (...args) => {
+    console.log("args", args)
 
     let sumAllArgs = 0
 
-    Array.from(arguments).forEach(item => sumAllArgs += item)
+    args.forEach(item => sumAllArgs += item)
 
     return sumAllArgs
 }
