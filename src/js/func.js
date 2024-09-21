2940d98b1cfe8b1709e5fcdfdfaf232830464710
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-09-21 12:01:24 +0330

    export defult

--- src/js/func.js
+++ src/js/func.js
@@ -1,31 +1,34 @@
-const users = [
-    {id: 18, name: 'Ali', age: 22},
-    {id: 29, name: 'Amin', age: 23},
-    {id: 36, name: 'Amir', age: 19},
-    {id: 41, name: 'Sasan', age: 28},
-    {id: 54, name: 'Qadir', age: 20},
-]
-
-export let usersCount = users.length
-
-export function isLogin(userID) {
-    let isUserInUsers = users.some(user => user.id === userID)
-
-    return isUserInUsers
-}
-
-export function userRegister(name, age) {
-    let newUserObj = {
-        id: Math.floor(Math.random() * 100),
-        name: name,
-        age: age
-    }
-
-    users.push(newUserObj)
-
-    return users
-
-}
+// const users = [
+//     {id: 18, name: 'Ali', age: 22},
+//     {id: 29, name: 'Amin', age: 23},
+//     {id: 36, name: 'Amir', age: 19},
+//     {id: 41, name: 'Sasan', age: 28},
+//     {id: 54, name: 'Qadir', age: 20},
+// ]
+//
+// export let usersCount = users.length
+//
+// export function isLogin(userID) {
+//     let isUserInUsers = users.some(user => user.id === userID)
+//
+//     return isUserInUsers
+// }
+//
+// export function userRegister(name, age) {
+//     let newUserObj = {
+//         id: Math.floor(Math.random() * 100),
+//         name: name,
+//         age: age
+//     }
+//
+//     users.push(newUserObj)
+//
+//     return users
+//
+// }
 
 
 // export { isLogin, userRegister, usersCount }
+
+
+export default usersCount
--- src/js/script.js
+++ src/js/script.js
@@ -641,17 +641,22 @@
 // console.log(userToken)
 
 
-import {isLogin, userRegister, usersCount as usersLength} from './func.js'
+// import {isLogin, userRegister, usersCount as usersLength} from './func.js'
+//
+//
+// let newUserName = prompt('Enter User Name: ')
+// let newUserAge = prompt('Enter User Age: ')
+//
+// console.log('Users Count:', usersLength);
+//
+// // console.log(userRegister(newUserName, newUserAge));
+//
+// console.log(isLogin(41));
 
 
-let newUserName = prompt('Enter User Name: ')
-let newUserAge = prompt('Enter User Age: ')
+import mohammad from "./func.js"
 
-console.log('Users Count:', usersLength);
-
-// console.log(userRegister(newUserName, newUserAge));
-
-console.log(isLogin(41));
+console.log("users count: ", mohammad)
 
 
 
